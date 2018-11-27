21795a65bbe9ffb3c14c5b77365dcb78f956a001
nohtaesang <nohtaesang@naver.com>
2018-11-28 02:21:20 +0900

    build write frame

--- client/src/js/components/section/write/Write.js
+++ client/src/js/components/section/write/Write.js
@@ -12,7 +12,6 @@ class ConnectedWrite extends Component {
             value: '',
         };
         this.handleChange = this.handleChange.bind(this);
-        this.myRef = React.createRef();
         this.command = this.command.bind(this);
     }
 
@@ -22,20 +21,55 @@ class ConnectedWrite extends Component {
         });
     }
 
-    command() {
+    command(e) {
         const success = document.execCommand('bold', false, null);
+        console.log(e.target.name);
     }
 
     render() {
         return (
             <div id="write">
                 <div id="options">
-                    <button type="button" onClick={this.command}>
+                    <button type="button" name="bold" onClick={this.command}>
                         {'bold'}
+                    </button>
+                    <button type="button" name="italic" onClick={this.command}>
+                        {'italic'}
+                    </button>
+                    <input type="number" />
+                    <button type="button" name="setFontSize" onClick={this.command}>
+                        {'setFontSize'}
+                    </button>
+                    <button type="button" name="insertHorizontalRule" onClick={this.command}>
+                        {'insertHorizontalRule'}
+                    </button>
+                    <button type="button" name="insertImage" onClick={this.command}>
+                        {'insertImage'}
+                    </button>
+                    <button type="button" name="justifyLeft" onClick={this.command}>
+                        {'justifyLeft'}
+                    </button>
+                    <button type="button" name="justifyCenter" onClick={this.command}>
+                        {'justifyCenter'}
+                    </button>
+                    <button type="button" name="justifyRight" onClick={this.command}>
+                        {'justifyRight'}
+                    </button>
+                    <button type="button" name="justifyFull" onClick={this.command}>
+                        {'justifyFull'}
+                    </button>
+                    <button type="button" name="redo" onClick={this.command}>
+                        {'redo'}
+                    </button>
+                    <button type="button" name="undo" onClick={this.command}>
+                        {'undo'}
+                    </button>
+                    <button type="button" name="removeFormat" onClick={this.command}>
+                        {'removeFormat'}
                     </button>
                 </div>
                 <div id="content">
-                    <div id="textField" contentEditable="true" ref={this.myRef} />
+                    <div id="textField" contentEditable="true" />
                 </div>
                 <div id="hashTag">hashTag</div>
                 <button type="submit" id="submit">
