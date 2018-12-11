e478e57e24709161ba4aa7c888c98213fbe9faed
gyofeel <gyofeel318@gmail.com>
2018-12-11 13:54:52 +0900

    add font awesome object

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-	faBold, faImage, faUndo, faRedo
+	faBold, faImage, faUndo, faRedo, faTimes
 } from '@fortawesome/free-solid-svg-icons';
 import Write from './section/write/Write';
 
-library.add(faBold, faImage, faUndo, faRedo);
+library.add(faBold, faImage, faUndo, faRedo, faTimes);
 
 const App = () => (
 	<div id="app">
