71d7e04e7a03dfc84370b3d9e90ed22aece57503
nohtaesang <nohtaesang@naver.com>
2018-12-11 14:51:34 +0900

    build cardlist form in dev

--- server/src/main/java/com/example/springboot_webservice/service/HelloSvc.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot_webservice.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springboot_webservice.entity.HelloDao;
-
-public interface HelloSvc extends JpaRepository<HelloDao, Integer> {
-
-}
